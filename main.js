e8a9d077137170344540f4f5cf40fdddfce8a1cb
Ryan <Ryan@301-4-6.local>
2018-05-23 14:03:31 -0400

    progress

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 var music = [{ 
-    albumName: "",
-    image: "",
+    albumName: "Beerbongs & Bentleys",
+    image: "https://consequenceofsound.files.wordpress.com/2018/05/aec-rpblb002821802-2_0.jpg?quality=80&w=380&h=380&crop=1",
     tracks: [ {
-        name : "",
-        length: "",
-        artist: "",
-        lyrics: ""
+        name : "Physcho",
+        length: "3:57",
+        artist: "Post Malone",
+        lyrics: "sound/pyscho.mp3"
         } ]
 }];
+
 
 var audioPyscho = new Audio('sound/pyscho.mp3');
 
 function onClickPlay(){
-    audioPyscho.play();
-    $("img").attr("src", "https://consequenceofsound.files.wordpress.com/2018/05/aec-rpblb002821802-2_0.jpg?quality=80&w=380&h=380&crop=1");
-    $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
+    music.forEach(function(a, num){
+        a[num].tracks[0].lyrics.play(); // set lyrics to audio
+        $("img").attr("src", a[0].image);
+        $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
+    })
+    // audioPyscho.play();
+    // $("img").attr("src", music[0].image);
+    // $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
 }
 
 function onClickPause(){
