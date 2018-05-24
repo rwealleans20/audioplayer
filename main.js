fe28e9b363622bdd768108ff854daa820358c72f
Ryan <Ryan@301-4-6.local>
2018-05-24 15:09:14 -0400

    progress

--- main.js
+++ main.js
@@ -6,24 +6,48 @@ var music = [{
         length: "3:57",
         artist: "Post Malone",
         lyrics: "sound/pyscho.mp3"
-        } ]
+    },{
+        name : "Lion",
+        length: "3:57",
+        artist: "Post Malone",
+        lyrics: "sound/pyscho.mp3"
+    }, {
+        name : "Hello",
+        length: "3:57",
+        artist: "Post Malone",
+        lyrics: "sound/pyscho.mp3"
+    } ]
 }];
 
+function buildPlaylist(){
+    music[0].tracks.forEach(function(a, num){
+        var str = "";
 
-var audioPyscho = new Audio('sound/pyscho.mp3');
+        str += `<div class="songPlaylist" onclick="onClickPlay(${num})">${a.name}</div>`;
 
-function onClickPlay(){
-    music.forEach(function(a, num){
-        a[num].tracks[0].lyrics.play(); // set lyrics to audio
-        $("img").attr("src", a[0].image);
-        $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
+        $(".playlist").append(str);
     })
-    // audioPyscho.play();
-    // $("img").attr("src", music[0].image);
-    // $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
 }
 
-function onClickPause(){
-    audioPyscho.pause();
-    $(".stopMusic").html(`<div class="playMusic"><h1 class="audioPause" onclick="onClickPlay()">Play</h1></div>`);
+function onClickPlay(num){
+    alert(num);
 }
+
+buildPlaylist();
+
+// var audioPyscho = new Audio('sound/pyscho.mp3');
+
+// function onClickPlay(){
+
+
+//     // audioPyscho.play();
+//     // $("img").attr("src", music[0].image);
+//     // $(".playMusic").html(`<div class="stopMusic"><h1 class="pause" onclick="onClickPause()">Pause Music</h1></div>`);
+// }
+
+// function onClickPause(){
+//     audioPyscho.pause();
+//     $(".stopMusic").html(`<div class="playMusic"><h1 class="audioPause" onclick="onClickPlay()">Play</h1></div>`);
+// }
+
+
